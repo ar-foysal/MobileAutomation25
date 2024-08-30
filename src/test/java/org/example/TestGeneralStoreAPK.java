d76c1a83d5b49925b2db80e0c9f3fd633d2a3809
arfoysal <arfoysal.me@gmail.com>
2024-08-30 22:14:08 +0600

    Install and uninstall application, scroll

--- src/test/java/org/example/TestGeneralStoreAPK.java
+++ src/test/java/org/example/TestGeneralStoreAPK.java
@@ -1,18 +1,27 @@
 package org.example;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
 public class TestGeneralStoreAPK {
 
-    @Test
-    public void testApp() throws MalformedURLException, InterruptedException {
+    public AndroidDriver driver;
+    @BeforeSuite
+    public AndroidDriver startApp() throws MalformedURLException {
+        File f = new File("src/test/resources");
+        File apk = new File(f, "General-Store.apk");
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("appium:deviceName", "batch22");
         desiredCapabilities.setCapability("udid", "emulator-5554");
@@ -20,14 +29,35 @@ public class TestGeneralStoreAPK {
         desiredCapabilities.setCapability("appium:automationName", "UiAutomator2");
         desiredCapabilities.setCapability("appium:appPackage", "com.androidsample.generalstore");
         desiredCapabilities.setCapability("appium:appActivity", "com.androidsample.generalstore.SplashActivity");
+        desiredCapabilities.setCapability("appium:app", apk.getAbsolutePath());
+
         URL remoteUrl = new URL("http://127.0.0.1:4723");
 
-        AndroidDriver driver = new AndroidDriver(remoteUrl, desiredCapabilities);
+        driver = new AndroidDriver(remoteUrl, desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        return driver;
+    }
 
+    @AfterSuite
+    public void quiteApp(){
+        driver.removeApp("com.androidsample.generalstore");
+//        driver.quit();
+    }
+
+    @Test
+    public void testApp() throws MalformedURLException, InterruptedException {
+        driver.findElement(By.id("com.androidsample.generalstore:id/spinnerCountry")).click();
+        WebElement bd_option = driver.findElement(AppiumBy.androidUIAutomator(
+                        "new UiScrollable(new UiSelector().scrollable(true).instance(0)).scrollIntoView(new UiSelector().text(\"Bangladesh\"))"));
+        bd_option.click();
         driver.findElement(By.id("com.androidsample.generalstore:id/nameField")).sendKeys("BD");
         driver.findElement(By.id("com.androidsample.generalstore:id/radioFemale")).click();
         driver.findElement(By.id("com.androidsample.generalstore:id/btnLetsShop")).click();
+        driver.findElement(By.xpath("//*[@text='Air Jordan 4 Retro']/following-sibling::android.widget.LinearLayout/*[@text='ADD TO CART']")).click();
+        String cart_count = driver.findElement(By.id("com.androidsample.generalstore:id/counterText")).getText();
+        Assert.assertEquals(cart_count, "1");
+        driver.findElement(By.id("com.androidsample.generalstore:id/appbar_btn_cart")).click();
+        driver.findElement(By.className("android.widget.CheckBox")).click();
         Thread.sleep(5000);
     }
 }
